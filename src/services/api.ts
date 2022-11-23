3fc093ffa0f4f647934653eed27728776906c8a7
BragaMPedro <pedrobmagalhaes95@gmail.com>
2022-11-23 01:25:37 -0300

    (refactor): Service métodos

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,20 +1,57 @@
-import axios from 'axios'
+import axios from "axios"
 
 export const tmdbApi = axios.create({
-    baseURL: 'https://api.themoviedb.org/3/',
-    headers:{
-        Accept:'application/json'
-    },
-    params: {
-        api_key: '387d1928e4dc591cdfacdc56a1a56d93'
-    }
-});
+   baseURL: "https://api.themoviedb.org/3/",
+   headers: {
+      Accept: "application/json",
+   },
+   params: {
+      api_key: "387d1928e4dc591cdfacdc56a1a56d93",
+      language: "pt-BR",
+      watch_region: "BR",
+   },
+})
 
+//GUEST SESSION_GET ------------------------------------------------------
+
+export function criaGuestSession() {
+   return tmdbApi.get("/authentication/guest_session/new")
+}
+
+//GUEST SESSION_POST ------------------------------------------------------
+
+export function postsClassificacao(movie_id) {
+   return tmdbApi.post(`/movie/${movie_id}/rating`)
+}
+
+//GUEST SESSION_DELETE ------------------------------------------------------
+
+export function deleteClassificacao(movie_id) {
+   return tmdbApi.delete(`/movie/${movie_id}/rating`)
+}
+
+
+//GET ------------------------------------------------------
 export function getPopularMovies() {
-    // tmdbApi.get("movie/popular").then((res) => {
-    //   console.log(res.data.results)
-    // }).catch((err) => {
-    //   console.log(err)
-    // })
-    return tmdbApi.get("movie/popular");
-  }
+   return tmdbApi.get("movie/popular")
+}
+
+export function discoverMovies() {
+   return tmdbApi.get("/discover/movie")
+}
+
+export function getStreams() {
+   return tmdbApi.get("/watch/providers/movie")
+}
+
+export function getMovieById(movie_id) {
+   return tmdbApi.get(`/movie/${movie_id}`)
+}
+
+export function getTranslations(movie_id) {
+   return tmdbApi.get(`/movie/${movie_id}/translations`)
+}
+
+export function getFilmesReconmendados(movie_id) {
+   return tmdbApi.get(`/movie/${movie_id}/similar`)
+}
