fc3937e491dfffa90ee8ed34faf39b4a8556a5d7
BragaMPedro <pedrobmagalhaes95@gmail.com>
2022-11-23 14:46:02 -0300

    finaliza StackNavigation

--- src/screens/SplashScreen/SplashScreen.tsx
+++ src/screens/SplashScreen/SplashScreen.tsx
@@ -1,12 +1,17 @@
-import Lottie from "lottie-react-native"
 import React from "react"
+import Lottie from "lottie-react-native"
 import { View } from "react-native"
 import { styles } from "./styles"
 
-export const SplashScreen = () => {
+export const SplashScreen = ({ navigation: { navigate } }) => {
    return (
       <View style={styles.container}>
-         <Lottie autoPlay source={require("../../assets/leCineSplashScreen.json")} />
+         <Lottie
+            autoPlay
+            source={require("../../assets/leCineSplashScreen.json")}
+            onAnimationFinish={() => navigate("HomeScreen")}
+            loop={false}
+         />
       </View>
    )
 }
