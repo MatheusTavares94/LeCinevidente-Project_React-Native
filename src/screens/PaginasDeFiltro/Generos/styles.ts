100958e2e7b93750f86022b3355969e96db778ec
MatheusTavares94 <mmatheus_ttavares@hotmail.com>
2022-11-23 19:53:10 -0300

    fix: Genre styles

--- src/screens/PaginasDeFiltro/Generos/styles.ts
+++ src/screens/PaginasDeFiltro/Generos/styles.ts
@@ -1,16 +1,38 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
 import themes from "../../../themes";
 import Constants from "expo-constants"
 
 export const styles = StyleSheet.create({
-    Container: {       
+    container:{
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        paddingHorizontal: 30,
+        width: Dimensions.get("screen").width,
         paddingTop: Constants.statusBarHeight,
-        flexDirection:'row',   
+        // backgroundColor: themes.COLORS.Roxo.escuro,
     },
     
-    cardText: {        
-        fontSize: 18,
-        color: "white"
-    }
+    titleContainer:{
+        width: Dimensions.get("screen").width,
+        height: 110,
+        padding: 20
+    },
+
+    title:{
+        textAlign: "center",
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: themes.COLORS.Roxo.maisClaro,
+    },
+
+    buttonContainer:{
+        zIndex:1,
+        position: 'absolute',
+        right: 50,
+        bottom: 0,
+    
+
+    },
 
 })
