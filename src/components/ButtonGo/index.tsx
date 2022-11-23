937de0e6cf307a8505e381310e5360e28c9caa77
GiseleBr <britto.gisele@gmail.com>
2022-11-23 14:56:21 -0300

    mudança de cor

--- src/components/ButtonGo/index.tsx
+++ src/components/ButtonGo/index.tsx
@@ -1,12 +1,17 @@
 import React from "react"
 import { Text, TouchableOpacity } from 'react-native'
 import { styles } from "./styles"
+import { useNavigation } from '@react-navigation/native';
 
-export const ButtonGo = () => {
+export const ButtonGo = ({next}) => {
+
+    const nav = useNavigation();
 
     return (
         <TouchableOpacity
+            
             style={styles.button}
+             onPress = {()=>nav.navigate(next)}
              >
             <Text
                 style={styles.buttonText}
