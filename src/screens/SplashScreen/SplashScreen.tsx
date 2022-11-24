f6b72969799e472e74010b39f90ebed8e2d7324b
BragaMPedro <pedrobmagalhaes95@gmail.com>
2022-11-24 17:15:08 -0300

    SplashSplashScreen

--- src/screens/SplashScreen/SplashScreen.tsx
+++ src/screens/SplashScreen/SplashScreen.tsx
@@ -1,17 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 import Lottie from "lottie-react-native"
 import { View } from "react-native"
 import { styles } from "./styles"
 
 export const SplashScreen = ({ navigation: { navigate } }) => {
+   const [showLeVidente, setShowLeVidente] = useState<boolean>(false)
+
    return (
       <View style={styles.container}>
          <Lottie
             autoPlay
             source={require("../../assets/leCineSplashScreen.json")}
-            onAnimationFinish={() => navigate("HomeScreen")}
+            onAnimationFinish={() => setShowLeVidente(true)}
             loop={false}
          />
+         {showLeVidente && (
+            <Lottie
+               autoPlay
+               source={require("../../assets/LeCineName.json")}
+               onAnimationFinish={() => navigate("HomeScreen")}
+               loop={false}
+            />
+         )}
       </View>
    )
 }
