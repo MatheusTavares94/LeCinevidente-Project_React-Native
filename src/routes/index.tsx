a33ed4b8151c308919c2cb92ad3b60a3f70394d2
Luiza Marins <luiza.marins@gmail.com>
2022-11-24 01:24:15 -0300

    go

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -25,7 +25,6 @@ export type RootTabParamList = {
 export type RootStackParamList = {
    SplashScreen: undefined
    HomeScreen: undefined
-   Genero: undefined
    Streams: undefined
    Generos: undefined
    Ano: undefined
