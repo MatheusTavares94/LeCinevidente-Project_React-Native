1833ead8c63fff2262593c5775f4758c07eefd68
MatheusTavares94 <mmatheus_ttavares@hotmail.com>
2022-11-24 15:52:56 -0300

    feat: year params

--- src/screens/PaginasDeFiltro/Anos/index.tsx
+++ src/screens/PaginasDeFiltro/Anos/index.tsx
@@ -1,31 +1,35 @@
-import React, { useState } from "react"
+import React, { useContext, useState } from "react"
 import { Text, TextInput, View } from "react-native"
 import { ButtonGo } from "../../../components/ButtonGo"
+import { ListaContexto } from "../../../context/listaContexto"
 import { styles } from "./styles"
 
 export const Ano = () => {
   const [ano, setAno] = useState<number>();
 
-  function enviaAno() {
-    
+  const setSearchParam = useContext(ListaContexto).setSearchParams
+  const searchParam = useContext(ListaContexto).searchParams
+
+  function addYear() {
+    setSearchParam({...searchParam, year: ano})
     console.log(ano)
   }
 
   return (
-      <View style={styles.Container}>
-          <Text style={styles.Text}>
-            digite o ano que deseja procurar
-          </Text>
+    <View style={styles.Container}>
+      <Text style={styles.Text}>
+        digite o ano que deseja procurar
+      </Text>
 
-        
-          <TextInput style={styles.textInput} keyboardType={"numeric"} maxLength={4}
-          onChangeText={(ano) => setAno(parseInt(ano))}/>
 
-          <View style={styles.buttonContainer}>
-            <ButtonGo title="Avançar" next={"OrderBy"} />
-         </View>
-      
+      <TextInput style={styles.textInput} keyboardType={"numeric"} maxLength={4}
+        onChangeText={(ano) => setAno(parseInt(ano))} />
+
+      <View style={styles.buttonContainer}>
+        <ButtonGo title="Avançar" next={"OrderBy"} metodoExtra={addYear} />
       </View>
+
+    </View>
   )
 
 }
